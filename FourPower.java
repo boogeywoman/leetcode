f40ce4fb8961ec786f478ad7267feb7efe591390
laibaamirislam <amirlaiba546@gmail.com>
2024-03-17 01:19:55 -0700

    Power Of Four

---
+++ FourPower.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/power-of-four/
+class FourPower {
+    public boolean isPowerOfFour(int n) {
+      if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+            if (n % 4 != 0) {
+                return false;
+            }
+            n /= 4;
+        }
+        return true;   
+    }
+}
